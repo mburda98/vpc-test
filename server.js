079a86d03b869e921bf3d2bdd29c0f82a78627f8
mburda98 <m.burda0805@gmail.com>
2021-09-21 12:15:23 +0200

    docker with push

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const port = 80;
 
 app.get("/", (req, res) => {
   console.log("hejo");
-  res.send("Hello World!");
+  res.send("Hello World from auto docker!");
 });
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
